99da391ca31a52b4a5353a8341deeb02d7ea9bc5
Elaine Chan <elaine.chan@outlook.com>
2016-06-16 17:11:45 -0400

    debug webpack config problem

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
 
 	devtool: 'source-map',
 	
-	entry: ['babel-polyfill', './app/js/app.jsx'],
+	entry: ['babel-polyfill', './app/shootings.js'],
 	
 	output: {
 		path: path.resolve(__dirname, 'public'),
