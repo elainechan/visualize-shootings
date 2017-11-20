5379ca0b2662011280de75427ac12867905a1dd0
Elaine Chan <elaine.chan@outlook.com>
2017-11-20 01:10:51 -0500

    csv parse

--- plotly/main.js
+++ plotly/main.js
@@ -1,6 +1,12 @@
 'use strict';
 
+// choropleth representing number killed in each state in 2016-2017
 
-{}
+// get "State" from each incident
+d3.csv("./shootings2016to2017.csv", data => {
+    console.log(data[0]);
+});
 
-var requestURL = "https://api.mapbox.com/geocoding/v5/{mode}/{query}.json?access_token=pk.eyJ1IjoiY2hhbnZlcnN1cyIsImEiOiJjamE3cmJxdDMxMTU5MzJsbDdlM2d5OGFqIn0.Od7n9c17-jouVVYbaMWOsg"
+console.log("hello");
+
+var requestURL = "https://api.mapbox.com/geocoding/v5/{mode}/{query}.json?access_token=pk.eyJ1IjoiY2hhbnZlcnN1cyIsImEiOiJjamE3cmJxdDMxMTU5MzJsbDdlM2d5OGFqIn0.Od7n9c17-jouVVYbaMWOsg";
