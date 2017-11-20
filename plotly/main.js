8ab18b4aaabe31dcc82716a879dbaf3497bac9b9
Elaine Chan <elaine.chan@outlook.com>
2017-11-20 00:48:12 -0500

    data cleaning

---
+++ plotly/main.js
@@ -0,0 +1,6 @@
+'use strict';
+
+
+{}
+
+var requestURL = "https://api.mapbox.com/geocoding/v5/{mode}/{query}.json?access_token=pk.eyJ1IjoiY2hhbnZlcnN1cyIsImEiOiJjamE3cmJxdDMxMTU5MzJsbDdlM2d5OGFqIn0.Od7n9c17-jouVVYbaMWOsg"
